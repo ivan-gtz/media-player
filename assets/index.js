20a98a7fd0b3bc30eae3196e559d41529f9cad41
Ivan Gutierrez <ivangutierz20@gmail.com>
2020-05-21 08:30:42 -0400

    clase 6 plugin

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
-import mediaPlayer from './mediaPlayer.js'
+import mediaPlayer from './mediaPlay.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
+const buttonMute = document.getElementById("mute")
 
-const player = new mediaPlayer({ el: video });
+const player = new mediaPlayer({ el: video, plugins: [new AutoPlay()] });
 button.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
