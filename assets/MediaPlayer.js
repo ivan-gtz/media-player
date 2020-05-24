ab445dfce366c37f4869a10e8bcb42996578855c
Ivan Gutierrez <ivangutierz20@gmail.com>
2020-05-24 03:32:18 -0400

    clase 23 Service Worked

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,10 +1,10 @@
-function mediaPlayer(config) {
+function MediaPlayer(config) {
   this.media = config.el;
   this.plugins = config.plugins || [];
   this._initPlugins();
 }
 
-mediaPlayer.prototype._initPlugins = function () {
+MediaPlayer.prototype._initPlugins = function () {
   const player = {
     play: () => this.play(),
     pause: () => this.pause(),
@@ -22,28 +22,28 @@ mediaPlayer.prototype._initPlugins = function () {
     plugin.run(player);
   });
 };
-mediaPlayer.prototype.play = function () {
+MediaPlayer.prototype.play = function () {
   this.media.play();
 };
 
-mediaPlayer.prototype.pause = function () {
+MediaPlayer.prototype.pause = function () {
   this.media.pause();
 };  
-mediaPlayer.prototype.togglePlay = function () {
+MediaPlayer.prototype.togglePlay = function () {
   this.media.paused 
     ? this.play() 
     : this.pause()
 };
-mediaPlayer.prototype.mute = function () {
+MediaPlayer.prototype.mute = function () {
   this.media.muted = true;
 };
-mediaPlayer.prototype.unmute = function () {
+MediaPlayer.prototype.unmute = function () {
   this.media.muted = false;
 };
-mediaPlayer.prototype.toggleMute = function () {
+MediaPlayer.prototype.toggleMute = function () {
   this.media.muted
     ? this.unmute()
     : this.mute() 
 };
 
-export default mediaPlayer;
+export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,20 @@
-import mediaPlayer from './mediaPlay.js'
+import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
 const buttonMute = document.getElementById("mute")
 
-const player = new mediaPlayer({ el: video, plugins: [
-  new AutoPlay()
+const player = new MediaPlayer({ el: video, plugins: [
+  new AutoPlay(),
+  new AutoPause()
 ] });
 button.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  })
+}
