8e78dc9400f0cc18e04e96065ba9e0f1d3e63dbd
Ivan Gutierrez <ivangutierz20@gmail.com>
2020-05-25 00:45:27 -0400

    clase 26 tipos basicos y funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,97 @@
-console.log('Hello, TypeScript');
+//bolean
+let muted: boolean = true;
+muted = false;
 
-function add(a:number, b:number) {
+//Numeros
+let numerador: number = 42;
+let denominador: number= 5;
+let resultado = numerador / denominador;
+
+//String
+let nombre: string = 'Ivan'
+let saludo = `Me llamo ${nombre}`;
+
+//Arreglos
+
+let people: string[] = [];
+people = ["Helen", "Raul", "Fernando"];
+people.push("999")
+
+let peopleAndNumbers: Array<string | number> = [];
+
+//  Enum
+
+enum Color {
+  Rojo = 'Rojo',
+
+  Verde = 'Verde',
+
+  Azul = 'Azul',
+}
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es ${colorFavorito}`)
+
+//any
+
+let comodin: any = "joker";
+comodin = {type: "Wildcard"};
+
+//Object
+
+let someObject: object = {type: "Wildcard"};
+
+//funciones
+
+function add(a: number, b: number): number {
   return a + b;
 }
 
-const  sum =add(5, 3)
+const sum = add(4, 8);
+
+function createAdder (a: number): (number) => number{
+  return function(b: number) {
+    return b + a;
+  }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+
+function fullName(firstName: string, lastName: string='Guti'): string{
+  return `${firstName} ${lastName}`;
+}
+
+const ivan = fullName("Ivan");
+
+console.log(ivan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
