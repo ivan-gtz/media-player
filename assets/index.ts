d6d123f01e52b8fe74c0cfd1bf27dfb818446887
Ivan Gutierrez <ivangutierz20@gmail.com>
2020-05-27 21:13:53 -0400

    plugin ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads'
 
 const video = document.querySelector("video")
 const button: HTMLElement = document.querySelector("button")
@@ -8,7 +9,8 @@ const buttonMute: HTMLElement = document.getElementById("mute")
 
 const player = new MediaPlayer({ el: video, plugins: [
   new AutoPlay(),
-  new AutoPause()
+  new AutoPause(),
+  new Ads(),
 ] });
 button.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
